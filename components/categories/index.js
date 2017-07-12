6f086e5d5b20d9971a050c2cb988a1beb455d920
Mariano Eiberman <mariano.eiberman@globant.com>
2017-07-12 14:58:17 -0300

    ...

--- components/categories/index.js
+++ components/categories/index.js
@@ -1,4 +1,48 @@
 import React from 'react';
+export const NEW_CATEGORY = 'NEW';
+export const EDIT_CATEGORY = 'EDIT';
+export const DELETE_CATEGORY = 'DELETE';
+export const VIEW_CATEGORY = 'VIEW';
+export class Categories extends React.Component {
 
-const CategoriesComponent = () => (<div>EXPENSES</div>);
-export default CategoriesComponent;
+    constructor(props) {
+        super(props);
+
+    }
+
+    gotoAction() {
+        if(this.props.action===NEW_CATEGORY) {
+            return this.newCategory();
+        }
+        if(this.props.action===EDIT_CATEGORY) {
+            return this.editCategory();
+        }
+        if(this.props.action===DELETE_CATEGORY) {
+            return this.deleteCategory()
+        }                
+        if(this.props.action===VIEW_CATEGORY) {
+            return this.viewCategories()
+        }        
+        return this.viewCategories();
+    }
+
+    newCategory() {
+        return <form>
+            <input name="ctg_name" />
+            <input name="ctg_description" />
+            <input name="ctg_currency" />
+        </form>
+    }
+
+    editCategory() {
+
+    }
+
+    deleteCategory() {
+
+    }
+    
+    render(){
+        return <div>{this.gotoAction()}</div>;
+    }
+}
--- components/expenses/index.js
+++ components/expenses/index.js
@@ -1,17 +1,44 @@
 import React from 'react';
+import {NEW_CATEGORY, EDIT_CATEGORY, DELETE_CATEGORY, Categories}  from '../categories';
+import { BrowserRouter as Router, Route } from 'react-router'
 
 class ExpensesComponent extends React.Component {
     constructor(props){
         super(props);
+        this.renderCategory = this.renderCategory.bind(this);
+        this.renderModal = this.renderModal.bind(this);
+        this.state = {
+            MODAL: ''
+        }
+    }
+
+    getExpenses() {
+
+    }
+
+    renderModal() {
+        if(this.state.MODAL === 'Categories') {
+            return <Categories action={this.state.action} />
+        }
+        return null;
+    }
+    newExpense() {}
+
+    renderCategory(e, action) {
+        this.setState({MODAL: 'Categories', action});
     }
 
     render() {
         const actions =  (this.props.userData.login) ? 
-                    <div> <button>NEW</button><button>PRINT</button></div> :
+                    <div> 
+                         <button>NEW Expenses</button>
+                         <button onClick={(e) => this.renderCategory(e, NEW_CATEGORY)} >New Category</button>
+                    </div> :
                     <div>You have to log in to interact</div>;
         return <div  className='expenses'>
             <div> Expenses </div>
             {actions}
+            {this.renderModal()}
         </div>
     }
 }
